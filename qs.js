37b7e68d5ac0355344184cf35c101b3fe7f87eea
Alex Morega <alex@grep.ro>
2015-07-14 18:12:27 +0200

    no ES6 deconstructing syntax

--- qs.js
+++ qs.js
@@ -4,7 +4,7 @@
     var rv = {};
     if(url.indexOf('?') > -1) {
       url.match(/\?(.*)/)[1].split('&').forEach(function(pair) {
-        var [k, v] = pair.split('=').map(decodeURIComponent);
+        var kv = pair.split('=').map(decodeURIComponent), k = kv[0], v = kv[1];
         if(! rv[k]) { rv[k] = []; }
         rv[k].push(v);
       });
